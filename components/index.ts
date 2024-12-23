af428a1cabf7f68694d1d5c82e788f423241c48d
Jummanbd <jummanahomedbd@gmail.com>
2024-12-23 14:11:35 +0600

    car-api website

---
+++ components/index.ts
@@ -0,0 +1,20 @@
+import CarCard from "./CarCard"
+import CarDetails from "./CarDetails"
+import CustomButton from "./CustomButton"
+import CustomFilter from "./CustomFilter"
+import Footer from "./Footer"
+import Hero from "./Hero"
+import NavBar from "./Navbar"
+import SearchBar from "./SearchBar"
+import ShowMore from "./ShowMore"
+export {
+  CarCard,
+  CarDetails,
+  CustomButton,
+  CustomFilter,
+  Footer,
+  Hero,
+  NavBar,
+  SearchBar,
+  ShowMore,
+}
